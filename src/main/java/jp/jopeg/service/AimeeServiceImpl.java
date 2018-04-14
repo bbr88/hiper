890cc67164e536263d95fd3991639c50d6285aa6
Igor Bisarnov <bisarnov92@gmail.com>
2018-04-14 16:44:29 +0300

    fix

--- src/main/java/jp/jopeg/service/AimeeServiceImpl.java
+++ src/main/java/jp/jopeg/service/AimeeServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.http.*;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -16,6 +17,7 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 import static jp.jopeg.controller.constants.QuestionsWithAnswers.AUTO_NEWS;
+import static jp.jopeg.controller.constants.QuestionsWithAnswers.AUTO_POSITIVE_ANSWER;
 
 /**
  * @author Igor Bisarnov
@@ -24,43 +26,61 @@ import static jp.jopeg.controller.constants.QuestionsWithAnswers.AUTO_NEWS;
 
 @Service
 public class AimeeServiceImpl implements AimeeService {
+    private static final String URL = "http://40.91.223.127:4560/answer/";
 
     private final RestTemplate restTemplate = new RestTemplate();
-
-    private static final String URL = "http://40.91.223.127:4560/answer/";
+    private boolean isStart = true;
 
     @Override
     public String askQuestion(String question) throws IOException {
         if ("start".equalsIgnoreCase(question)) {
             return AUTO_NEWS.getQuestion();
         }
+        question = isStart ? AUTO_NEWS.getQuestion() + " " + question : question;
+        isStart = false;
+
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL).queryParam("key", question);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL).queryParam("key", extendQuestion(question));
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
+        try {
+            HttpEntity<String> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class);
 
-        HttpEntity<String> response = restTemplate.exchange(
-            builder.toUriString(),
-            HttpMethod.GET,
-            entity,
-            String.class);
+            ObjectMapper objectMapper = new ObjectMapper();
+            TypeFactory typeFactory = objectMapper.getTypeFactory();
+            CollectionType collectionType = typeFactory.constructCollectionType(List.class, QuestionResponseDto.class);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        TypeFactory typeFactory = objectMapper.getTypeFactory();
-        CollectionType collectionType = typeFactory.constructCollectionType(
-            List.class, QuestionResponseDto.class);
+            List<QuestionResponseDto> questionListDto = objectMapper.readValue(response.getBody(), collectionType);
 
-        List<QuestionResponseDto> questionListDto = objectMapper.readValue(response.getBody(), collectionType);
-
-        if (questionListDto != null && !questionListDto.isEmpty()) {
-//            questionListDto.sort(Comparator.comparing(QuestionResponseDto::getScore));
-            questionListDto.sort((o1, o2) -> o2.getScore().compareTo(o1.getScore()));
-            return questionListDto.get(0).getAnswer();
-        } else {
-            return "nihuya";
+            if (questionListDto != null && !questionListDto.isEmpty()) {
+                questionListDto.sort((o1, o2) -> o2.getScore().compareTo(o1.getScore()));
+                return questionListDto.get(0).getAnswer();
+            } else {
+                return AUTO_POSITIVE_ANSWER.getAnswer();
+            }
+        } catch (HttpClientErrorException ex) {
+            return AUTO_POSITIVE_ANSWER.getAnswer();
         }
     }
+
+    private String extendQuestion(String question) {
+        if (!StringUtils.isEmpty(question)) {
+            if (question.toLowerCase().contains("нет")) {
+                return question + " против";
+            }
+            if (question.toLowerCase().contains("да")) {
+                return question + " да давай";
+            }
+            return question;
+        }
+        return "да давай";
+    }
+
 }
